c749fd88b00a7691d365f13552364ed019ba07e6
Gonçalo Serrano <aluno25948@ipt.pt>
2025-05-05 11:48:49 +0100

    Base De Dados Melhorada

---
+++ Ludix/Ludix/Data/LudixContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ludix.Models;
+
+namespace Ludix.Data
+{
+    public class LudixContext : DbContext
+    {
+        public LudixContext (DbContextOptions<LudixContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ludix.Models.Review> Review { get; set; } = default!;
+    }
+}
--- Ludix/Ludix/Models/Developer.cs
+++ Ludix/Ludix/Models/Developer.cs
@@ -1,10 +1,14 @@
-﻿namespace Ludix.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ludix.Models
 {
     /// <summary>
     /// Tabela que representa um desenvolvedor de jogos sendo este uma expecializacao de um user
     /// </summary>
-    public class Developer : User
+    public class Developer 
     {
+        [Key]
+        public int id { get; set; }
         /// <summary>
         /// Website do desenvolvedor
         /// </summary>
